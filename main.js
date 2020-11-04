5094763a7a60e4cdbd7bdc18886033e1d8536265
ponarasu-developer <73824607+ponarasu-developer@users.noreply.github.com>
2020-11-04 23:25:39 +0530

    time laplase

---
+++ main.js
@@ -0,0 +1,37 @@
+
+let year;
+let months ;
+let days ;
+let hours ;
+let minutes;
+let seconds ;
+function cal() {
+  let input = document.getElementById("birthyear").value;
+
+ year =(2020 - input);
+ months =((2020-input)*12);
+ days = ((2020-input)*365);
+hours = ((2020-input)*365*24);
+minutes = ((2020-input)*365*24*60);
+seconds = ((2020-input)*365*24*60*60);
+
+let yearanswer = document.getElementById('years');
+yearanswer.textContent=(`NO OF YEAR'S YOU SPEND ON EARTH:${year}`);
+
+let monthsanswer = document.getElementById('months');
+monthsanswer.textContent = (`NO OF MONTH'S YOU SPEND ON EARTH:${months}`);
+
+let daysanswer = document.getElementById('days');
+
+ daysanswer.textContent = (`NO OF DAY'S YOU SPEND ON EARTH:${days}`);
+
+let hoursanswer = document.getElementById('hours');
+hoursanswer.textContent = (`NO OF HOUR'S YOU SPEND ON EARTH:${hours}`);
+
+let minutesanswer = document.getElementById('minutes');
+minutesanswer.textContent = (`NO OF MINUTE'S YOU SPEND ON EARTH:${minutes}`);
+
+let secondsanswer = document.getElementById('seconds');secondsanswer.textContent = (`NO OF SECOND'S YOU SPEND ON EARTH:${seconds}`);
+console.log(input);
+}
+
