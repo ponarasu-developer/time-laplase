63c246d264d88284bb6f00c78567864172cb10fd
ponarasu-developer <73824607+ponarasu-developer@users.noreply.github.com>
2020-11-05 12:13:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,21 @@ let minutes;
 let seconds ;
 function cal() {
   let input = document.getElementById("birthyear").value;
+if (input > 2020 ) {
+   oops = document.getElementById("opps");
+  oops.textContent =("OOPS YOU ENTER THE INVALID YEAR");
+  
+}
+else if(input<0){
+    oops = document.getElementById("opps");
+  oops.textContent =("OOPS YOU ENTER THE INVALID YEAR");
+  
+}
 
- year =(2020 - input);
+else{
+
+
+   year =(2020 - input);
  months =((2020-input)*12);
  days = ((2020-input)*365);
 hours = ((2020-input)*365*24);
@@ -34,4 +47,7 @@ minutesanswer.textContent = (`NO OF MINUTE'S YOU SPEND ON EARTH:${minutes}`);
 let secondsanswer = document.getElementById('seconds');secondsanswer.textContent = (`NO OF SECOND'S YOU SPEND ON EARTH:${seconds}`);
 console.log(input);
 }
+
+}
+
 
